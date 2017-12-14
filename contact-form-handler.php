17c6faa8cbd49e5ae4d1ca48edc73fa7f03d3b03
Michal <michal_sachar@o2.pl>
2017-12-14 18:40:32 +0100

    zainstalowany composer, phpmailer, skrypt validateForm - CZĘŚĆ_2 - wcześniejszy commit nie zawierał wszystkich plików

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,26 +1,84 @@
-<? php
-$errors = '';
-$myEmail = 'michal_sachar@o2.pl';
-if (empty($_POST['input-name']) || empty($_POST['input-email']) || empty($_POST['input-message'])){
-	$errors .= '\n wszystkie pola są wymagane'
+<?php
+
+require __DIR__ . '\vendor\autoload.php';
+// require __DIR__ . '\vendor\phpmailer\phpmailer\PHPMailerAutoload.php';
+
+function sendEmail($config){
+	$mail = new PHPMailer();
+	try {
+		$mail->CharSet = 'UTF-8';
+		//Server settings
+		// $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+		// $mail->SMTPDebug = 3;                                 // Enable verbose debug output
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host = 'mailtrap.io';  // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = '87ad9968ae1160';                 // SMTP username
+		$mail->Password = '4a31abf3ba91f5';                           // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = 25;                                    // TCP port to connect to
+
+		//Recipients
+		$mail->setFrom('michal_sachar@o2.pl', 'Michał Sacharczuk');
+		$mail->addAddress('michal_sachar@o2.pl', 'Michał Sacharczuk');     // Add a recipient
+		// $mail->addAddress('ellen@example.com');               // Name is optional
+		$mail->addReplyTo($config->from_email, $config->from_name);
+		// $mail->addCC('cc@example.com');
+		// $mail->addBCC('bcc@example.com');
+
+		//Attachments
+		// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+		//Content
+		$mail->isHTML(true);                                  // Set email format to HTML
+		$mail->Subject = 'Nowa wiadomość ze strony PORTFOLIO';
+		$mail->Body    = 'Nadawca: ' . $config->from_name .
+						' &lt;' . $config->from_email . '&gt; napisał:<br><br>' . $config->message;
+
+		$html = new \Html2Text\Html2Text($mail->Body);
+		$mail->AltBody = $html->getText();
+
+		$mail->send();
+
+		?>
+		<script>
+			sessionStorage.mailSuccess = true;
+		</script>
+		<?php
+
+		// echo 'Message has been sent';
+	} catch (Exception $e) {
+
+		?>
+		<script>
+			sessionStorage.mailSuccess = false;
+		</script>
+		<?php
+
+		// echo 'Message could not be sent.';
+		// echo 'Mailer Error: ' . $mail->ErrorInfo;
+	}
 }
 
-$name = $_POST('input-name');
-$email = $_POST('input-email');
-$message = $_POST('input-message');
 
-if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*\.[a-z]{2,3}$/i", $email)){
-	$errors .= '\n adres e-mail jest niepoprawny'
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	$config = (object) [
+		'from_email' => $_POST['input-email'],
+		'from_name' => $_POST['input-name'],
+		'message' => $_POST['input-message']
+	];
+
+	sendEmail($config);
+	// header('Location: index.html');
+
+	?>
+	<script>
+		sessionStorage.mailSent = true;
+		window.location = 'index.html';
+	</script>
+	<?php
+
 }
 
-if(!empty($errors)){
-	$subject = 'Nowa wiadomość ze strony PORTFOLIO \n';
-	$body = 'Nadawca: $name \n E-mail: $email \n Treść wiadomości: \n $message \n';
-	$headers = 'Od: $myEmail \n Odpowiedź do: $email';
-	mail($myEmail, $subject, $body, $headers);
-
-	header('Location: index.html');
-	// print('<script>alert("Wiadomość wysłano");</script>');
-	print("<script>document.querySelector('#envelope-top').classList.add('closedEnvelope');</script>");
-}
-?>
+?>	
